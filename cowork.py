b0129e457ccb7b5f89aac414b0d4f4db94b4f908
woongg <bgu090u@gmail.com>
2024-02-29 12:07:15 +0900

    revise readme

--- cowork.py
+++ cowork.py
@@ -4,9 +4,10 @@ Please feel free to add!
 """
 
 
-def JiwonBaek():
+def JiwonBaek(a):
     print('Hello this is Jiwon Baek')
-    return 0
+    a = a + 1
+    return a
 
 
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Requirement
+
+##Version
+python ver 3.7
+
+This project is for:
+- des
+- devs
+- SD
